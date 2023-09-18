c86d64360bd4e8f34ad9299d3d041cd5ce3ff76b
rudqhr22 <rudqhr22@naver.com>
2023-09-18 17:30:18 +0900

    DevicePreview Add&Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
-/*
+
 void main() {
   runApp(
     DevicePreview(
@@ -9,8 +9,8 @@ void main() {
     ),
   );
 }
-*/
 
+/*
 void main() {
   runApp(
     const Center(
@@ -21,7 +21,7 @@ void main() {
     ),
   );
 }
-
+*/
 
 /*
 void main() {

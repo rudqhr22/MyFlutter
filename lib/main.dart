5041beaa086b90de902bba57d11932eb8c0f3dde
rudqhr22 <rudqhr22@naver.com>
2023-09-20 13:41:12 +0900

    Row Study

--- lib/main.dart
+++ lib/main.dart
@@ -56,67 +56,33 @@ class MyApp extends StatelessWidget {
             child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.lightGreen,
-                  border: Border.all(
-                    width: 16,
-                    color: Colors.black
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
                   ),
-                  borderRadius: BorderRadius.circular(10.0)
-                ),
-                height: 100,
-                width: 100,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.lightGreen,
-                  border: Border.all(
-                      width: 16,
-                      color: Colors.blue
+                  SizedBox(width: 100),
+                  Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.cyanAccent,
                   ),
-                  borderRadius: BorderRadius.circular(10.0)
-                ),
-                height: 100,
-                width: 100,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.lightGreen,
-                    border: Border.all(
-                        width: 16,
-                        color: Colors.red
-                    ),
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-                height: 100,
-                width: 100,
-              ),
-              SizedBox(
-                width: 200.0,
-                height: 100.0,
-                child: Container(
-                  color: Colors.yellow,
-                  height: 40,
-                  width: 80,
-                ),
-              ),
-              Container(
-                color: Colors.red,
-                margin: const EdgeInsets.all(40.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Container(
-                    color: Colors.indigoAccent,
-                    width: 50,
-                    height: 50,
-                  ),
-                ),
-                ),
+                  SizedBox(width: 100),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                  )
+                ],
+              )
             ],
+            )
           ),
         ),
-      ),
       ),
     );
   }
